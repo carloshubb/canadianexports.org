3c90c3948f600a4c9811e62133857b68e80f862b
carloslennix <carloslennix38@gmail.com>
2026-01-08 23:08:48 -0400

    rechange

--- resources/js/web/coffee-wall/Create.vue
+++ resources/js/web/coffee-wall/Create.vue
@@ -78,7 +78,7 @@
                             </label>
                         </div>
 
-                        <!-- Quarterly c-->
+                        <!-- Quarterly-->
                         <div class="w-full">
                             <label
                                 class="w-full block cursor-pointer px-5 py-5 text-base md:text-2xl font-FuturaMdCnBT text-center border rounded-none rounded-r-md md:rounded-r-lg"
