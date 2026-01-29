4841520248871c9bd9a7e82581dbeb4d837ac84d
carloslennix <carloslennix38@gmail.com>
2026-01-29 10:05:25 -0400

    cors error

--- config/cors.php
+++ config/cors.php
@@ -20,12 +20,14 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:8000',
-        'https://seashell-squirrel-840185.hostingersite.com',
-    ],
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:8000',
+
+    'https://canadianexports.org',
+    'https://www.canadianexports.org',
+        ],
 
     'allowed_origins_patterns' => [],
 
